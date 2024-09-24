b0cec380f801b7f6659670f37334d1bb65720b96
Minerva <minerva.gonzalez@chasacademy.se>
2024-09-24 12:23:06 +0200

    Dagens_Uppgift09_23.py

---
+++ Dagens_Uppgift09_23.py
@@ -0,0 +1,22 @@
+#Test om olika form print variablertest om olika variabler
+CPU = 35
+MEM = 30
+DISK = 60
+# första exempel
+print(CPU)
+print(MEM)
+print(DISK)
+
+# andra exempel
+
+print(f"{CPU = }")
+print(f'{MEM = }')
+print(f'DISK = {DISK}')
+
+# tredje exempel
+
+print(f'{CPU = }, {MEM = }, {DISK = }')
+
+# färde exempel
+
+print(f"CPU = {CPU}\nMEM = {MEM}\nDISK = {DISK}\n")
